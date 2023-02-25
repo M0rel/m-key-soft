b66cec79e64129150ea74d2508af4b79c99033c9
Vadym Kharchuk <vadimharchuk@gmail.com>
2023-02-25 18:34:37 +0200

    Add USB report generating and sending
    Signed-off-by: Vadym Kharchuk <vadimharchuk@gmail.com>

--- Core/Src/core/core.c
+++ Core/Src/core/core.c
@@ -1,11 +1,26 @@
-#include "core.h"
 #include <stdbool.h>
+
+#include "usbd_hid.h"
+#include "usb_device.h"
+
+#include "core.h"
+#include "keyboard.h"
+
+key_report_st_t report;
+extern USBD_HandleTypeDef hUsbDeviceFS;
 
 int user_main(void)
 {
+        HAL_Delay (3000);
         while (true) {
+                HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 
+                key_input_to_usb_report(&report);
+
+                USBD_HID_SendReport(&hUsbDeviceFS, &report,
+                                    sizeof(key_report_st_t));
         };
 
         return 0;
 }
+
--- Core/Src/core/core.h
+++ Core/Src/core/core.h
@@ -1,6 +1,8 @@
 #ifndef __CORE_H__
 #define __CORE_H__
 
+#include "stm32f4xx_hal.h"
+
 int user_main(void);
 
 #endif /* __CORE_H__ */
